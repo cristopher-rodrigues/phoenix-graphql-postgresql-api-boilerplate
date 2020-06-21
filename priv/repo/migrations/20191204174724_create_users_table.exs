273cfbb1ab9edb461a9af95c48a2f43946a4e1de
cristopher.santos <cristopher.santos@pipefy.com>
2020-06-21 17:23:10 -0300

    STASH

--- priv/repo/migrations/20191204174724_create_users_table.exs
+++ priv/repo/migrations/20191204174724_create_users_table.exs
@@ -3,8 +3,7 @@ defmodule Boilerplate.Repo.Migrations.CreateUserTable do
 
   def change do
     create table(:users, primary_key: false) do
-      add :id, :bigserial, primary_key: true, null: false
-      add :uuid, :binary_id, null: false
+      add :uuid, :uuid, null: false, primary_key: true
       add :name, :string, null: false, limit: 150
       add :removed_at, :utc_datetime_usec
       timestamps(type: :utc_datetime_usec)
---
+++ terraform/vpc.tf
@@ -0,0 +1,17 @@
+vpc.tf
+
+
+-backend-config="prod-infra.config"
+
+prod-infra.config
+  region=?
+  key="?"
+  bucket=?
+
+  VPC OUTPUT???
+
+
+graph
+
+data vpc?
+k8s apps (kops) vpc?
